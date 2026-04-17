92a5a8433755e7d2c500e7518ccad06486243c11
Philip Nye <philipnye@users.noreply.github.com>
2026-04-17 13:59:08 +0100

    Switch to using new views

--- csps_extraction/sql/select_organisations_data.sql
+++ csps_extraction/sql/select_organisations_data.sql
@@ -1,61 +1,75 @@
+-- NB: case statements do two things:
+    -- 1. Add ' - <yyyy> iteration' strings that were cleaned as part of the extraction and loading of the data into the database back in to organisation names, to facilitate comparison between the collated data generated using this script and that in the original working file
+    -- 2. (Specific to the CSPS organisations data) Handle organisations with type 'Aggregation' or 'Disaggregation' that feature in the source data, as these don't feature in civil_service.vw_organisation_departmental_group and civil_service.vw_organisation_latest
 select
     cspso.id,
     cspso.headline_category [Headline category],
     cspso.year [Year],
-    cspso.organisation [Organisation],
     case
-        when o.organisation is null then 'Y'
+        when cspso.organisation_name = 'Department for Culture, Media and Sport' and o.end_year = 2017 then 'Department for Culture, Media and Sport - 2017 iteration'
+        when cspso.organisation_name = 'Department for Culture, Media and Sport' and o.start_year = 2024 then 'Department for Culture, Media and Sport - 2024 iteration'
+        when cspso.organisation_name = 'Ministry of Housing, Communities & Local Government' and o.start_year = 2018 then 'Ministry of Housing, Communities & Local Government - 2018 iteration'
+        when cspso.organisation_name = 'Ministry of Housing, Communities & Local Government' and o.start_year = 2024 then 'Ministry of Housing, Communities & Local Government - 2024 iteration'
+        else cspso.organisation_name
+    end [Organisation],
+    case
+        when o.name = 'All employees' then 'Y'
+        when o.name is null then 'Y'
         else null
     end [Organisation aggregation?],
-    case cspso.organisation
+    case cspso.organisation_name
         when 'All employees' then 'All employees'
-        when 'Civil Service benchmark' then 'Civil Service benchmark'
-        when 'Department for Work and Pensions, Jobcentre Plus and Pensions & Disability Carers Service' then 'DWP'
-        when 'Scotland, Wales and Northern Ireland Offices, and the Office of the Advocate General for Scotland' then 'Various'
-        when 'Ministry of Justice group (including agencies)' then 'MoJ'
-        when 'Ministry of Justice arm''s length bodies' then 'MoJ'
-        when 'National Offender Management Service group (including agencies)' then 'MoJ'
-        when 'Historic Scotland and the Royal Commission on the Ancient and Historic Monuments of Scotland' then 'Scot Gov'
-        when 'UK Statistics Authority (excluding Office for National Statistics)' then 'CO'
-        when 'Department for Education group (including agencies)' then 'DfE'
-        when 'HM Prison and Probation Service (excluding HM Prison Service and National Probation Service/Probation Service)' then 'MoJ'
         when 'Cabinet Office group (including agencies)' then 'CO'
-        else o.departmental_group
+        when 'Civil Service benchmark' then 'Civil Service benchmark'
+        when 'Department for Education group (including agencies)' then 'DfE'
+        when 'Department for Work and Pensions, Jobcentre Plus and Pensions & Disability Carers Service' then 'DWP'
+        when 'Historic Scotland and the Royal Commission on the Ancient and Historic Monuments of Scotland' then 'Scot Gov'
+        when 'HM Prison and Probation Service (excluding HM Prison Service and National Probation Service/Probation Service)' then 'MoJ'
+        when 'Ministry of Justice arm''s length bodies' then 'MoJ'
+        when 'Ministry of Justice group (including agencies)' then 'MoJ'
+        when 'National Offender Management Service group (including agencies)' then 'MoJ'
+        when 'Scotland, Wales and Northern Ireland Offices, and the Office of the Advocate General for Scotland' then 'Various'
+        when 'UK Statistics Authority (excluding Office for National Statistics)' then 'CO'
+        else vodg.departmental_group_short_name
     end [Departmental group],
     case
-        when o.organisation is null then 'Combination'
-        else o.organisation_type
+        when o.name is null then 'Combination'
+        else o.type
     end [Organisation type],
-    case cspso.organisation
+    case cspso.organisation_name
         when 'All employees' then 'All employees'
-        when 'Civil Service benchmark' then 'Civil Service benchmark'
-        when 'Department for Work and Pensions, Jobcentre Plus and Pensions & Disability Carers Service' then 'Department for Work and Pensions, Jobcentre Plus and Pensions & Disability Carers Service'
-        when 'Scotland, Wales and Northern Ireland Offices, and the Office of the Advocate General for Scotland' then 'Scotland, Wales and Northern Ireland Offices, and the Office of the Advocate General for Scotland'
-        when 'Ministry of Justice group (including agencies)' then 'Ministry of Justice group (including agencies)'
-        when 'Ministry of Justice arm''s length bodies' then 'Ministry of Justice arm''s length bodies'
-        when 'National Offender Management Service group (including agencies)' then 'National Offender Management Service group (including agencies)'
-        when 'Historic Scotland and the Royal Commission on the Ancient and Historic Monuments of Scotland' then 'Historic Scotland and the Royal Commission on the Ancient and Historic Monuments of Scotland'
-        when 'UK Statistics Authority (excluding Office for National Statistics)' then 'UK Statistics Authority (excluding Office for National Statistics)'
-        when 'Department for Education group (including agencies)' then 'Department for Education group (including agencies)'
-        when 'HM Prison and Probation Service (excluding HM Prison Service and National Probation Service/Probation Service)' then 'HM Prison and Probation Service (excluding HM Prison Service and National Probation Service/Probation Service)'
         when 'Cabinet Office group (including agencies)' then 'Cabinet Office group (including agencies)'
-        else lo.latest_organisation
-    end [Latest organisation],
-    case cspso.organisation
-        when 'All employees' then 'All employees'
         when 'Civil Service benchmark' then 'Civil Service benchmark'
-        when 'Department for Work and Pensions, Jobcentre Plus and Pensions & Disability Carers Service' then 'DWP'
-        when 'Scotland, Wales and Northern Ireland Offices, and the Office of the Advocate General for Scotland' then 'Various'
-        when 'Ministry of Justice group (including agencies)' then 'MoJ'
-        when 'Ministry of Justice arm''s length bodies' then 'MoJ'
-        when 'National Offender Management Service group (including agencies)' then 'MoJ'
-        when 'Historic Scotland and the Royal Commission on the Ancient and Historic Monuments of Scotland' then 'Scot Gov'
-        when 'UK Statistics Authority (excluding Office for National Statistics)' then 'CO'
-        when 'Department for Education group (including agencies)' then 'DfE'
-        when 'HM Prison and Probation Service (excluding HM Prison Service and National Probation Service/Probation Service)' then 'MoJ'
+        when 'Department for Education group (including agencies)' then 'Department for Education group (including agencies)'
+        when 'Department for Work and Pensions, Jobcentre Plus and Pensions & Disability Carers Service' then 'Department for Work and Pensions, Jobcentre Plus and Pensions & Disability Carers Service'
+        when 'Historic Scotland and the Royal Commission on the Ancient and Historic Monuments of Scotland' then 'Historic Scotland and the Royal Commission on the Ancient and Historic Monuments of Scotland'
+        when 'HM Prison and Probation Service (excluding HM Prison Service and National Probation Service/Probation Service)' then 'HM Prison and Probation Service (excluding HM Prison Service and National Probation Service/Probation Service)'
+        when 'Ministry of Justice arm''s length bodies' then 'Ministry of Justice arm''s length bodies'
+        when 'Ministry of Justice group (including agencies)' then 'Ministry of Justice group (including agencies)'
+        when 'National Offender Management Service group (including agencies)' then 'National Offender Management Service group (including agencies)'
+        when 'Scotland, Wales and Northern Ireland Offices, and the Office of the Advocate General for Scotland' then 'Scotland, Wales and Northern Ireland Offices, and the Office of the Advocate General for Scotland'
+        when 'UK Statistics Authority (excluding Office for National Statistics)' then 'UK Statistics Authority (excluding Office for National Statistics)'
+        else vol.latest_organisation_name
+    end [Latest organisation],
+    case cspso.organisation_name
+        when 'All employees' then 'All employees'
         when 'Cabinet Office group (including agencies)' then 'CO'
-        else lo.latest_departmental_group
-    end [Latest departmental group],
+        when 'Civil Service benchmark' then 'Civil Service benchmark'
+        when 'Department for Education group (including agencies)' then 'DfE'
+        when 'Department for Work and Pensions, Jobcentre Plus and Pensions & Disability Carers Service' then 'DWP'
+        when 'Historic Scotland and the Royal Commission on the Ancient and Historic Monuments of Scotland' then 'Scot Gov'
+        when 'HM Prison and Probation Service (excluding HM Prison Service and National Probation Service/Probation Service)' then 'MoJ'
+        when 'Ministry of Justice arm''s length bodies' then 'MoJ'
+        when 'Ministry of Justice group (including agencies)' then 'MoJ'
+        when 'National Offender Management Service group (including agencies)' then 'MoJ'
+        when 'Scotland, Wales and Northern Ireland Offices, and the Office of the Advocate General for Scotland' then 'Various'
+        when 'UK Statistics Authority (excluding Office for National Statistics)' then 'CO'
+        else iif(
+            vol.latest_organisation_name = 'Indeterminate',
+            vol.latest_determinate_organisation_short_name,
+            vol.latest_organisation_short_name
+        )
+    end [Latest IfG departmental group],
     cspso.section [Section],
     cspso.measure [Measure],
     cspso.label [Label],
@@ -64,10 +78,9 @@ select
     cspso.based_on [Based on],
     cspso.notes [Notes]
 from civil_service.civil_service_people_survey_organisations cspso
-    inner join civil_service.release_number rn on
-        cspso.year = rn.year and
-        rn.quarter = 4
     left join civil_service.organisation o on
         cspso.organisation_id = o.id
-    left join civil_service.vw_latest_organisation lo on
-        o.id = lo.organisation_id
+    left join civil_service.vw_organisation_departmental_group vodg on
+        o.id = vodg.organisation_id
+    left join civil_service.vw_organisation_latest vol on
+        vodg.ifg_departmental_group_id = vol.organisation_id
