6d71c39761fdcac97e147e3bd45a9f832521b29f
Philip Nye <philipnye@users.noreply.github.com>
2026-04-21 10:34:21 +0100

    Rename 'Latest IfG departmental group' col

--- csps_extraction/sql/select_organisations_data.sql
+++ csps_extraction/sql/select_organisations_data.sql
@@ -6,6 +6,7 @@
 -- NB: `case` statements do two things:
     -- 1. `Organisation` column: Add ' - <yyyy> iteration' strings that were cleaned as part of the extraction and loading of the data into the database back in to organisation names, to facilitate comparison between the collated data generated using this script and that in the original working file
     -- 2. (Specific to the CSPS organisations data) `Departmental group`, `Latest organisation`, `Latest IfG departmental group` columns: Handle organisations with type 'Aggregation' or 'Disaggregation' that feature in the source data, as these don't feature in civil_service.vw_organisation_departmental_group and civil_service.vw_organisation_latest
+-- NB: 'Latest IfG departmental group' is renamed 'Latest departmental group', so that existing PivotTables connections to collated datasets don't break
 with cspso as (
     select
         *,
@@ -101,7 +102,7 @@ select
             vol2.latest_determinate_organisation_short_name,
             vol2.latest_organisation_short_name
         )
-    end [Latest IfG departmental group],
+    end [Latest departmental group],
     cspso.section [Section],
     cspso.measure [Measure],
     cspso.label [Label],
